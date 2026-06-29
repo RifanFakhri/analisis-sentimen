40427104d70fb8b209e6ee25460d4368baed8260
fakhririfan86 <fakhririfan86@gmail.com>
2026-06-29 14:12:15 +0700

    revert default DB_PORT to 5432

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
         DB_USERNAME = os.getenv('DB_USERNAME', 'postgres')
         DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
         DB_HOST = os.getenv('DB_HOST', 'localhost')
-        DB_PORT = os.getenv('DB_PORT', '5433')
+        DB_PORT = os.getenv('DB_PORT', '5432')
         DB_NAME = os.getenv('DB_NAME', 'sentimen')
         SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     
